f57814b9ff75ca4dbe9fe07a1ff7fd2d6e3c43a6
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-11-24 12:58:16 -0600

    list products into admin panel

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from '../../services/service.index';
+import {ProductService, UserService} from '../../services/service.index';
 
 @Component({
   selector: 'app-admin',
@@ -9,9 +9,39 @@ import {UserService} from '../../services/service.index';
 export class AdminComponent implements OnInit {
 
 
-  constructor(public _userService: UserService) { }
+  constructor(public _userService: UserService, public _productService: ProductService) { }
+  page = 1;
+  products: any[] = [];
+  registerTotal = 0;
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+  loadProducts(): void {
+    this._productService.loadProducts(this.page)
+      .subscribe( (resp: any) => {
+        this.registerTotal = resp.meta.total;
+        this.products = resp.data;
+      });
+  }
+  changeFrom(from: number): void{
+    const fromTo = this.page + from;
+
+    if (fromTo >= this.registerTotal ){
+      return;
+    }
+
+    if (fromTo < 0 ) {
+      return;
+    }
+    this.page += from;
+    this.loadProducts();
+  }
+  update(id: string): void{
+
+  }
+  delete(id: string): void{
+
   }
 
 }
